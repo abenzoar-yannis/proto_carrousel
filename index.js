c1896c04590e9a09e89bba3e2730d18293523452
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-01-29 18:22:24 +0100

    Création de la boucle qui fait apparaitre toute mes images

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ let library = [
   '<img class="size" src="images/image03.jpg">',
   '<img class="size" src="images/image04.jpg">',
 ];
+let imageIndex = 0;
 
 const createImage = (i) => {
   insert.innerHTML = library[i];
   setTimeout(() => {
     insert.innerHTML = "";
-  }, 3000);
+  }, 6000);
 };
+
+const loop = () => {
+  setTimeout(() => {
+    if (imageIndex >= library.length) {
+      imageIndex = 0;
+      loop();
+    } else {
+      createImage(imageIndex);
+      imageIndex++;
+      setTimeout(() => {
+        loop();
+      }, 6000);
+    }
+  }, 20);
+};
+
+loop();
