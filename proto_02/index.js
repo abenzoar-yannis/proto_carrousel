0b8f5fd92409b0950972a2920d4db39a18ffe261
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-02-01 03:27:40 +0100

    Ajout des bouttons, de ma zone d'insertion et ma library d'images

---
+++ proto_02/index.js
@@ -0,0 +1,10 @@
+const insert = document.getElementById("insert");
+const previous = document.getElementById("slideLeft");
+const following = document.getElementById("slideRight");
+
+let library = [
+  { index: 1, content: '<img src="images/image01.jpg">' },
+  { index: 2, content: '<img src="images/image02.jpg">' },
+  { index: 3, content: '<img src="images/image03.jpg">' },
+  { index: 4, content: '<img src="images/image04.jpg">' },
+];
